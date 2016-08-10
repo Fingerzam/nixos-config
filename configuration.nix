89450c036ed4ddc8fb7f3db7469f0ea46fae69e0
Juhana Laurinharju <juhana.laurinharju@solita.fi>
2016-08-10 14:28:58 +0300

    nixos config

---
+++ configuration.nix
@@ -0,0 +1,220 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Most of this is based on bodil's configs
+  boot.cleanTmpDir = true;
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  # Define on which hard drive you want to install Grub.
+  boot.loader.grub.device = "/dev/sda";
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Select internationalisation properties.
+  i18n = {
+    consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "fi";
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Helsinki";
+
+  # Enable the X11 windowing system.
+  services.xserver = {
+    enable = true;
+    windowManager.i3.enable = true;
+    # displayManager.slim.enable = true;
+    desktopManager.xterm.enable = false;
+    startGnuPGAgent = true;
+    layout = "fi";
+    xkbOptions = "ctrl:nocaps,eurosign:e";
+  };
+
+  # Diable pc speaker
+  boot.blacklistedKernelModules = [ "snd_pcsp" ];
+  boot.extraModprobeConfig = ''
+    options snd slots=snd-hda-intel
+  '';
+
+  programs.ssh.startAgent = false;
+  virtualisation.virtualbox.host.enable = true;
+
+  nix.trustedBinaryCaches = [ https://hydra.nixos.org ];
+
+  services.udisks2.enable = true;
+  services.gnome3.at-spi2-core.enable = true;
+
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    neovim vim emacs joe git nodejs ponysay mu powertop
+    file inetutils lftp unzip xlibs.xev rsync
+
+    # fonts
+    source-code-pro
+
+    # essentials
+    tmux htop wget ponysay psmisc gptfdisk gnupg xclip xsel xdotool
+
+    # build essentials
+    binutils gcc gnumake pkgconfig python ruby
+
+    # desktop components
+    dmenu xlibs.xbacklight xscreensaver unclutter compton
+    networkmanagerapplet volumeicon pavucontrol feh
+    xlibs.xrandr liberation_ttf pavucontrol
+    libnotify gnome3.gnome_themes_standard
+    gnome3.adwaita-icon-theme gnome3.gsettings_desktop_schemas
+    acpi dunst jq i3status i3lock i3blocks
+
+    # desktop apps
+    chromium firefox kde5.okular gnome3.gnome_terminal hipchat
+    keepass
+
+    # console apps
+    apg stow
+
+  ];
+
+  fonts = {
+     enableFontDir = true;
+     enableGhostscriptFonts = true;
+     fonts = with pkgs; [
+       corefonts  # Micrsoft free fonts
+       inconsolata  # monospaced
+       ubuntu_font_family  # Ubuntu fonts
+       unifont # some international languages
+       source-code-pro #Adobe source code pro fonts
+     ];
+   };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  networking.firewall.allowedTCPPorts = [22];
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  hardware = {
+    pulseaudio.enable = true;
+    pulseaudio.support32Bit = true; # This might be needed for Steam games
+  };
+
+  # Enable Postgresql
+  services.postgresql.enable = true;
+  services.postgresql.package = pkgs.postgresql94;
+  services.postgresql.authentication = "local all all ident";
+
+
+  # Enable the KDE Desktop Environment.
+  services.xserver.displayManager.kdm.enable = true;
+  services.xserver.desktopManager.kde4.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.extraUsers.fingerzam = {
+    isNormalUser = true;
+    name = "fingerzam";
+    uid = 1000;
+    extraGroups = [ "wheel" "disk" "audio" "video" "networkmanager" "systemd-journal" ];
+
+  };
+
+  # The NixOS release to be compatible with for stateful data such as databases.
+  system.stateVersion = "16.03";
+
+  environment.variables.GTK_DATA_PREFIX = "${pkgs.gnome3.gnome_themes_standard}";
+
+  environment.etc."gtk-3.0/settings.ini".text = ''
+    [Settings]
+    gtk-theme-name=Adwaita
+    gtk-icon-theme-name=gnome
+    gtk-font-name=Droid Sans 20
+    gtk-cursor-theme-name=Adwaita
+    gtk-cursor-theme-size=0
+    gtk-toolbar-style=GTK_TOOLBAR_BOTH
+    gtk-toolbar-icon-size=GTK_ICON_SIZE_LARGE_TOOLBAR
+    gtk-button-images=1
+    gtk-menu-images=1
+    gtk-enable-event-sounds=1
+    gtk-enable-input-feedback-sounds=1
+    gtk-xft-antialias=1
+    gtk-xft-hinting=1
+    gtk-xft-hintstyle=hintslight
+    gtk-xft-rgba=rgb
+  '';
+
+  environment.etc."fonts/local.conf".text = ''
+    <?xml version="1.0"?>
+    <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+    <fontconfig>
+
+      <match target="font">
+        <edit name="antialias" mode="assign">
+          <bool>true</bool>
+        </edit>
+      </match>
+      <match target="font">
+        <edit name="hinting" mode="assign">
+          <bool>true</bool>
+        </edit>
+      </match>
+       <match target="font">
+        <edit name="hintstyle" mode="assign">
+          <const>hintslight</const>
+        </edit>
+      </match>
+      <match target="font">
+        <edit name="rgba" mode="assign">
+          <const>rgb</const>
+        </edit>
+      </match>
+      <match target="font">
+        <edit mode="assign" name="lcdfilter">
+          <const>lcddefault</const>
+        </edit>
+      </match>
+      <match target="pattern">
+        <test qual="any" name="family"><string>Helvetica</string></test>
+        <edit name="family" mode="assign"><string>Droid Sans</string></edit>
+      </match>
+
+      <alias>
+        <family>sans-serif</family>
+        <prefer>
+          <family>Droid Sans</family>
+        </prefer>
+      </alias>
+      <alias>
+        <family>serif</family>
+        <prefer>
+          <family>Droid Serif</family>
+        </prefer>
+      </alias>
+      <alias>
+        <family>monospace</family>
+        <prefer>
+          <family>PragmataPro</family>
+        </prefer>
+      </alias>
+
+    </fontconfig>
+  '';
+}
+
